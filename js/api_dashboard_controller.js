dfdee9107c45719ae72e0c1d9d3337d6b68eeec7
nathanallen <thenathanator@gmail.com>
2016-02-09 16:37:05 -0800

    working http request w/ error message

--- js/api_dashboard_controller.js
+++ js/api_dashboard_controller.js
@@ -1,6 +1,6 @@
 console.log('api_dashboard_controller.js loaded!');
 
-FAIL_RATE = 0 // 0-100% percent change that $http requests will fail
+FAIL_RATE = 50 // 0-100% percent change that $http requests will fail
 
 app.controller("ApiDashboardController", ApiDashboardController)
 
@@ -15,9 +15,23 @@ function ApiDashboardController($http){
 
   this.api_endpoint = ""; // user inputed url
   this.display_data = null; // JSON response data
+  this.display_error = "";
 
   this.getEndpoint = function(){
-    // request the api endpoint
-    // display the response data
+
+    $http
+      .get(this.api_endpoint)
+      .then(
+        function onSuccess(response){
+          vm.display_error = "";
+          vm.display_data = response.data;
+        },
+
+        function onError(error){
+          vm.display_error = "Error - Request Failed"
+          vm.display_data = null;
+        }
+      );
+
   }
 }
